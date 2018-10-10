9004fb483b185ef99605807a5de28a63248c6dd8
Štěpán Veselý <stepanvesely@dhcp-237-098.eduroam.muni.cz>
2018-10-10 08:17:07 +0200

    Initial

---
+++ 03/import.py
@@ -0,0 +1,26 @@
+import sqlite3
+import os
+from scorelib import load
+import sys
+
+os.system("sqlite3 scorelib.dat < scorelib.sql")
+data = load(sys.argv[1])
+
+conn = sqlite3.connect('scorelib.dat')
+conn.text_factory = str
+
+c = conn.cursor()
+
+# store persons
+for record in data:
+    for author in (record.edition.composition.authors):
+        checker = False
+        for row in c.execute("Select name from person"):
+            if author.name == row[0]: checker = True
+        if checker == False: c.execute("INSERT INTO person(name, born, died) VALUES  (?, ?, ?)", (author.name, author.born, author.died))
+        conn.commit()
+
+
+
+for row in c.execute("Select name from person"):
+    print (row)
