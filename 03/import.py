c5d2d7a37faa8b7119e25816316a61008733486e
Štěpán Veselý <stepanvesely@Stepan-MacBook-Air.local>
2018-10-11 21:10:05 +0200

    Further

--- 03/import.py
+++ 03/import.py
@@ -3,6 +3,16 @@ import os
 from scorelib import load
 import sys
 
+def lookupCompoAuthor(data):
+    for author in (data.edition.composition.authors):
+        for row in c.execute("Select id from person where name=?", (author.name,)):
+            return(row[0])
+
+def storeScoreAuthor(compoID, authorID):
+    c.execute("INSERT INTO score_author(score, composer) VALUES  (?, ?)", (compoID, authorID))
+    conn.commit()
+
+
 os.system("sqlite3 scorelib.dat < scorelib.sql")
 data = load(sys.argv[1])
 
@@ -31,8 +41,21 @@ for record in data:
         conn.commit()
 
 
+# store composition
+for record in data:
+    c.execute("INSERT INTO score(name, genre, key, incipit, year) VALUES  (?, ?, ?, ?, ?)", (record.edition.composition.name, record.edition.composition.genre, record.edition.composition.key, record.edition.composition.incipit, record.edition.composition.year))
+    conn.commit()
+    compoAuthor = lookupCompoAuthor(record)
+    for item in (c.execute("Select max(id) from score")):
+        compoID = item[0]
+    storeScoreAuthor(compoID,compoAuthor)
 
+# store voices
+for record in data:
+    for index, voice in enumerate(record.edition.composition.voices):
+        c.execute("INSERT INTO voice(number, score, range, name) VALUES  (?, ?, ?, ?)", (index+1, compoID, voice.range, voice.name))
+        conn.commit()
 
-
-for row in c.execute("Select name from person"):
- print (row[0])
+#for row in c.execute("Select * from voice"):
+ #for line in row:
+#    print (line)
