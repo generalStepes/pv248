1cb3872ee93b7383b40089ba392b2e467a486d3d
stepes <veselydp@gmail.com>
2018-11-24 21:53:07 +0100

    Update http-forward.py

--- 09/http-forward.py
+++ 09/http-forward.py
@@ -1,17 +1,124 @@
-from aiohttp import web
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import path
+import http, ssl
+import json
 import sys
 
-async def handle(request):
-    print(request)
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
-    if request.body_exists:
-        print(await request.read())
+hostName = "localhost"
+hostPort = 9000
+responseDict = {}
 
-app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.post('/', handle),
-                web.get('/{name}', handle)])
+def httpClientFun(url,headers, timeout, content, requestType):
+        url, params = cleanAddress(url)
+        #print(headers)
+        try:
+            conn = http.client.HTTPSConnection(url, context=ssl._create_unverified_context(), timeout=timeout)
+            if requestType == "POST": conn.request(requestType, params, content, headers = headers)
+            else: conn.request(requestType, params, headers = headers)
+            r1 = conn.getresponse()
+        except:
+            return (None, None, None, "True")
+        headers = r1.getheaders()
+        status = r1.status
+        sourceCode = r1.read().decode("utf-8", "ignore")
+        conn.close()
+        return (headers, status, sourceCode, "False")
 
-web.run_app(app, port=sys.argv[1])
+def cleanAddress(url):
+    url = url.replace("https://","")
+    url = url.replace("http://","")
+    slash = url.find("/")
+    if slash != -1:
+        urlParsed = url[:slash]
+        try:
+            params = url[slash:]
+        except:
+            params = "/"
+    else:
+        urlParsed = url
+        params = "/"
+    return urlParsed, params
+
+def parseHeaders(headers):
+    headersDict = {}
+    for item in headers:
+        headersDict[item[0]] = item[1]
+    return headersDict
+
+def parseContent(sourceCode, status, connFailed):
+    if connFailed == "False":
+        responseDict["code"] = status
+        try:
+            sourceCodeJ = sourceCode.replace("\n", "")
+            responseDict["json"] = json.loads(sourceCodeJ)
+        except:
+            responseDict["content"] = sourceCode
+    if connFailed == "True":
+        responseDict["code"] = "timeout"
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        global responseDict
+        responseDict = {}
+        headers, status, sourceCode, connFailed = httpClientFun(sys.argv[2], None, 1, None, "GET")
+        if connFailed == "False": responseDict["headers"] = parseHeaders(headers)
+        parseContent(sourceCode, status, connFailed)
+        responseDictJ = str(json.dumps(responseDict))
+        responseDictJ = bytes(responseDictJ, 'utf-8')
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(responseDictJ)
+
+    def do_POST(self):
+        global responseDict
+        responseDict = {}
+        try:
+            load = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
+        except:
+            responseDict["code"] = "invalid json"
+            connFailed = "NotExist"
+            sourceCode = None
+            status = None
+        try:
+            requestType = load["type"]
+        except:
+            requestType = "GET"
+        if requestType == "": requestType = "GET"
+        try:
+            headers = load["headers"]
+        except:
+            headers = {}
+        if headers == "": headers = {}
+        try:
+            timeout = load["timeout"]
+        except:
+            timeout = 1
+        try:
+            url = load["url"]
+        except:
+            responseDict["code"] = "invalid json"
+            connFailed = "NotExist"
+            sourceCode = None
+            status = None
+        try:
+            content = load["content"]
+        except:
+            responseDict["code"] = "invalid json"
+            connFailed = "NotExist"
+            sourceCode = None
+            status = None
+        if "code" not in responseDict: headers, status, sourceCode, connFailed = httpClientFun(url, headers, timeout, content, requestType)
+        if connFailed == "False": responseDict["headers"] = parseHeaders(headers)
+        parseContent(sourceCode, status, connFailed)
+        responseDictJ = str(json.dumps(responseDict))
+        responseDictJ = bytes(responseDictJ, 'utf-8')
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(responseDictJ)
+
+
+myServer = HTTPServer((hostName, hostPort), RequestHandler)
+myServer.serve_forever()
