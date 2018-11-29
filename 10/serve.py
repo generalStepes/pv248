586a0c114051d18f06a24b296e0c0f5de29eb81d
Štěpán Veselý <stepanvesely@Stepan-MacBook-Air.local>
2018-11-29 12:53:45 +0100

    Update 10/serve.py

--- 10/serve.py
+++ 10/serve.py
@@ -1,14 +1,50 @@
-from aiohttp import web
+
+#!/usr/bin/env python3
+from socketserver import ThreadingMixIn
+from http.server import HTTPServer, CGIHTTPRequestHandler
+import socketserver
 import sys
+import urllib
+import os
+import logging
 
-async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+def parseFileName(item):
+    filePath = (sys.argv[2] + item.path)
+    fileSize = os.path.getsize(filePath)
+    return filePath, fileSize
+
+def processStuff(self):
+    filePath, fileSize = parseFileName(self)
+    if (filePath.endswith(".cgi")):
+            self.cgi_info = '', filePath
+            self.run_cgi()
+
+    if filePath.endswith(".cgi") == False:
+        self.send_response(200)
+        self.send_header('Content-Length', str(fileSize))
+        self.end_headers()
+        fp = open(filePath, 'rb')
+        read = 0
+        while True:
+            data = fp.read()
+            self.wfile.write(data)
 
 
-app = web.Application()
-app.add_routes([web.get('/{name}', handle),
-                web.post('/{name}', handle)])
 
-web.run_app(app, port = sys.argv[1])
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
+
+def createHandler():
+    class Handler(CGIHTTPRequestHandler):
+        def do_GET(self):
+            processStuff(self)
+        def do_POST(self):
+            processStuff(self)
+        def do_HEAD(self):
+            processStuff(self)
+    return Handler
+
+handler = createHandler()
+server = ThreadedHTTPServer(('localhost', int(sys.argv[1])), handler)
+print ('Starting server, use <Ctrl-C> to stop')
+server.serve_forever()
