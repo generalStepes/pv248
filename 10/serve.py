65c3855429a1f5a0e12607211c71ecf728e5e671
Štěpán Veselý <stepanvesely@Stepan-MacBook-Air.local>
2018-12-04 23:42:23 +0100

    Update 10/serve.py

--- 10/serve.py
+++ 10/serve.py
@@ -10,8 +10,16 @@ import logging
 import json
 
 def parseFileNameGet(item):
+    slash = item.path.find("/")
+    if slash != -1:
+        addr = item.path[:slash]
+        print(addr)
     filePath = (sys.argv[2] + item.path)
-    fileSize = os.path.getsize(filePath)
+    try:
+        fileSize = os.path.getsize(filePath)
+    except:
+        fileSize = None
+        filePath = None
     return filePath, fileSize
 
 def parseFileNamePost(item):
@@ -19,24 +27,50 @@ def parseFileNamePost(item):
     fileSize = os.path.getsize(filePath)
     return filePath, fileSize
 
+def findPath(relPath):
+    relPath = os.path.join(sys.argv[2], relPath)
+    path = os.path.abspath(relPath)
+    pathBool = os.path.isfile(path)
+    return path, relPath, pathBool
+
 def processStuff(self, requestType):
-    if requestType == "POST":
-        load = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
-        filePath, fileSize = parseFileNamePost(load["content"])
-    if requestType == "GET": filePath, fileSize = parseFileNameGet(self)
-    if (filePath.endswith(".cgi")):
-            self.cgi_info = '', filePath
+        parsedURL = urllib.parse.urlparse(self.path)
+        path, relPath, pathBool = findPath(parsedURL.path[1:])
+
+        if requestType == "GET":
+            paramsPath = relPath + "?" + parsedURL.query
+            paramsPath = paramsPath.strip("?")
+
+        if requestType == "POST":
+            load = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
+            params = ""
+            for item in load:
+                params += item + "=" + load[item] + "&"
+            try:
+                params += parsedURL.query
+            except:
+                pass
+            params = params.strip("&")
+            paramsPath = relPath + "?" + params
+            paramsPath = paramsPath.strip("?")
+            print(paramsPath)
+        if (pathBool == True and relPath is not None and relPath.endswith(".cgi")):
+            self.cgi_info = '', paramsPath
             self.run_cgi()
 
-    if filePath.endswith(".cgi") == False:
-        self.send_response(200)
-        self.send_header('Content-Length', str(fileSize))
-        self.end_headers()
-        fp = open(filePath, 'rb')
-        read = 0
-        while True:
-            data = fp.read()
-            self.wfile.write(data)
+        if pathBool == True and relPath is not None and relPath.endswith(".cgi") == False:
+            fileSize = os.path.getsize(relPath)
+            self.send_response(200)
+            self.send_header('Content-Length', str(fileSize))
+            self.end_headers()
+            fp = open(relPath, 'rb')
+            read = 0
+            while True:
+                data = fp.read()
+                self.wfile.write(data)
+        if pathBool == False:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
 
 
 
